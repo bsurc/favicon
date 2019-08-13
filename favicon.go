6fda624df23bad12214a239a4e3f5c8b002beb7c
Kyle Shannon <kyle@pobox.com>
2019-08-13 13:38:26 -0600

    fix package name

--- favicon.go
+++ favicon.go
@@ -1,4 +1,8 @@
-package main
+// Copyright (c) 2019, Boise State University All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package favicon
 
 import (
 	"net/http"
